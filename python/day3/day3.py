ebd80b9c19e9b657da4c3b1f6e0b516eb40159e9
Daniel Bergmann <bergmann.da@gmx.de>
2022-12-08 00:12:37 +0100

    day3B complete

--- python/day3/day3.py
+++ python/day3/day3.py
@@ -5,7 +5,6 @@ def priority_for_letter(item):
     ASCII_A = ord("A")
     ASCII_a = ord("a")
     ASCII_Z = ord("Z")
-    ASCII_z = ord("z")
     ascii_nr = ord(item)
     if ascii_nr > ASCII_Z:  # lower case
         prio = ascii_nr - ASCII_a + 1
@@ -24,8 +23,9 @@ def common_chars_3sets(set1, set2, set3):
     return common_chars
 
 
-input_file_path = r"./day3/input_test.txt"
+# input_file_path = r"./day3/input_test.txt"
 input_file_path = r"./day3/input.txt"
+
 file_object = open(input_file_path, "r")
 lines = file_object.readlines()
 lines = [line.split() for line in lines]
@@ -44,4 +44,14 @@ for line in lines:
     prios.append(priority_for_letter(common_chars_in_this_line[0]))
 print("day3A: ", sum(prios))
 
-# for idx in range(len(lines))
+badge_prios = 0
+for idx in range(len(lines)):
+    if idx % 3 == 0:
+        string1 = lines[idx][0]
+        string2 = lines[idx + 1][0]
+        string3 = lines[idx + 2][0]
+        badge_this_triplet = list(
+            common_chars_3sets(set(string1), set(string2), set(string3))
+        )
+        badge_prios += priority_for_letter(badge_this_triplet[0])
+print("day3B: ", badge_prios)

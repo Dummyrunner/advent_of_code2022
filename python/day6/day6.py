dd5994227ec9ea4a9350b3b1f34455f9908ce253
Daniel Bergmann <bergmann.da@gmx.de>
2022-12-11 14:43:30 +0100

    add files for day6

---
+++ python/day6/day6.py
@@ -0,0 +1,8 @@
+from ipdb import set_trace
+
+# input_file_path = r"./day6/input_test.txt"
+input_file_path = r"./day6/input.txt"
+
+
+file_object = open(input_file_path, "r")
+lines = file_object.readlines()

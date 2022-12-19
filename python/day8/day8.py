c3f5d60e0ca21cccfe8f918d020eac5946806760
Bergmann Daniel <Daniel.Bergmann3@bosch.com>
2022-12-19 12:36:51 +0100

    refactor bound checks in subroutines, clean debug output

--- python/day8/day8.py
+++ python/day8/day8.py
@@ -29,6 +29,30 @@ def get_val_from_point(grid, point_x, point_y):
     return int(grid[point_y][point_x])
 
 
+def is_on_boundary(grid, point_x, point_y):
+    NUM_OF_ROWS = len(grid)
+    NUM_OF_COLS = len(grid[0])
+    on_boundary = (
+        point_x == 0
+        or point_x == NUM_OF_COLS - 1
+        or point_y == 0
+        or point_y == NUM_OF_ROWS - 1
+    )
+    return on_boundary
+
+
+def is_out_of_bound(grid, point_x, point_y):  #
+    NUM_OF_ROWS = len(grid)
+    NUM_OF_COLS = len(grid[0])
+    out_of_bound = (
+        point_x < 0
+        or point_x > NUM_OF_COLS - 1
+        or point_y < 0
+        or point_y > NUM_OF_ROWS - 1
+    )
+    return out_of_bound
+
+
 def is_visible(grid, point_x, point_y, directions):
     """Check whether point given by coordinates point_x, point_y is visible in grid"""
     for dir in directions:
@@ -57,8 +81,6 @@ def is_visible(grid, point_x, point_y, directions):
 
 def is_visible_in_direction_rec(grid, curr_pos_x, curr_pos_y, start_val, direction):
     """Check recursively, whether point given by curr_pos is visible in direction "direction" """
-    # print("check direction\t", direction.name, "now (recursive)")
-
     NUM_OF_ROWS = len(grid)
     NUM_OF_COLS = len(grid[0])
     incr_x = direction.increment_x
@@ -68,40 +90,12 @@ def is_visible_in_direction_rec(grid, curr_pos_x, curr_pos_y, start_val, directi
     new_pos_x = curr_pos_x + incr_x
     new_pos_y = curr_pos_y + incr_y
 
-    new_pos_x_out_of_bound = new_pos_x < 0 or new_pos_x > NUM_OF_COLS - 1
-    new_pos_y_out_of_bound = new_pos_y < 0 or new_pos_y > NUM_OF_ROWS - 1
-
-    # print(
-    #     "curr: (",
-    #     curr_pos_x,
-    #     ",",
-    #     curr_pos_y,
-    #     ")\t new: (",
-    #     new_pos_x,
-    #     ",",
-    #     new_pos_y,
-    #     ")\t",
-    #     "posx out of b:",
-    #     new_pos_x_out_of_bound,
-    #     "; posy out of b:",
-    #     new_pos_y_out_of_bound,
-    # )
-    # set_trace()
-    if new_pos_x_out_of_bound or new_pos_y_out_of_bound:
+    if is_out_of_bound(grid, new_pos_x, new_pos_y):
         return True
     else:
         new_point_val = get_val_from_point(grid, new_pos_x, new_pos_y)
 
         if new_point_val >= start_val:
-            # print(
-            #     "point (",
-            #     curr_pos_x,
-            #     ",",
-            #     curr_pos_y,
-            #     ") NOT visible in dir ",
-            #     direction.name,
-            #     "due to higher tree",
-            # )
             return False
         else:
             print(
@@ -156,4 +150,3 @@ for irow in range(1, NUM_OF_ROWS - 1):
         if is_visible(grid, icol, irow, directions_NESW):
             visibile_ctr += 1
 print("EXC 8A: visible trees\t", visibile_ctr)
-# set_trace()

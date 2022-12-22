adec5c01ca32adfee9042c06f61b51a999d61205
Bergmann Daniel <Daniel.Bergmann3@bosch.com>
2022-12-22 15:33:08 +0100

    chaos and anarchy

--- python/day7/day7.py
+++ python/day7/day7.py
@@ -2,9 +2,16 @@
 from ipdb import set_trace
 
 
-class RootNode:
+class Node:
+    def __init__(self, name):
+        self.name = name
+
+
+class RootNode(Node):
     def __init__(self):
         self.children = []
+
+    name = "/"
 
 
 class NodeDirectory(RootNode):
@@ -14,7 +21,7 @@ class NodeDirectory(RootNode):
         self.parent = parent
 
 
-class NodeFile(NodeDirectory):
+class NodeFile(RootNode):
     def __init__(self, parent, name, file_size):
         super().__init__(parent, name)
         self.file_size = file_size
@@ -23,25 +30,19 @@ class NodeFile(NodeDirectory):
 class CLI:
     def __init__(self, root):
         self.root_node = root
-        self.# ipdb Debugger. add line set_trace() to enter debugger there
-from ipdb import set_trace
+        self.current_dir = root
 
 
-class RootNode:
-    def __init__(self):
-        self.children = []
-
-
-class NodeDirectory(RootNode):
+class NodeDirectory:
     def __init__(self, parent, name):
         self.children = []
         self.name = name
         self.parent = parent
 
 
-class NodeFile(NodeDirectory):
+class NodeFile:
     def __init__(self, parent, name, file_size):
-        super().__init__(parent, name)
+        self.children = []
         self.file_size = file_size
 
 
@@ -56,22 +57,34 @@ def parse_command7(command_string):
     command_string = command_string[2:]
 
 
-
 input_file_path = r"./day7/input_test.txt"
 # input_file_path = r"./day7/input.txt"
 
 
 def parseCommand(command_string):
     # remove leading $
-    command_string = command_string[1:]
-    if command_string[0:1] == "cd":
-        pass
-    elif command_string[0:1] == "ls":
-        pass
+    command_string = command_string[2:]
+    if command_string[0:2] == "cd":
+        cmd = "cd"
+    elif command_string[0:2] == "ls":
+        cmd = "ls"
+    else:
+        print("ERROR UNKNOWN COMMAND")
+    return (cmd, command_string[3:])
+
 
 def change_dir(destination):
-
+    pass
 
 
 input_file_path = r"./day7/input_test.txt"
 # input_file_path = r"./day7/input.txt"
+
+# Parse Input file
+file_object = open(input_file_path, "r")
+# remove \n from strings with strip()
+command_list = [x.strip() for x in file_object.readlines()]
+
+# print(command_list)
+first_command = command_list[0]
+print(parseCommand(first_command))

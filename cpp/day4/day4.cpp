3349947829d2d1475a2940a3ec2dd70af02008b1
Bergmann Daniel <Daniel.Bergmann3@bosch.com>
2023-03-23 17:06:26 +0100

    create Interval class

--- cpp/day4/day4.cpp
+++ cpp/day4/day4.cpp
@@ -27,6 +27,29 @@ std::pair<int, int> parseIntMinusIntToPair(const std::string &str) {
   return res;
 }
 
+template <typename TypeT> class Interval {
+public:
+  Interval(TypeT lower, TypeT upper) { setBounds(lower, upper); }
+  void setBounds(TypeT lower, TypeT upper) {
+    if (upper < lower) {
+      throw std::runtime_error("Upper bound is smaller than lower bound!");
+    }
+    m_upper_bound = upper;
+    m_lower_bound = lower;
+  }
+
+  bool includes(Interval &other) {
+    return (this.m_upper_bound >= other.m_upper_bound &&
+            this.m_lower_bound <= other.m_lower_bound);
+  }
+
+  // TODO bool intersects(Interval &other) {return false;}
+
+private:
+  TypeT m_upper_bound;
+  TypeT m_lower_bound;
+};
+
 int main() {
   // std::string path = "./day4/input_test.txt";
   std::string path = "./day4/input.txt";
